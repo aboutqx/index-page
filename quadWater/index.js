a02d942ed72d03c8772962fa1493a103eb9d84c4
aboutqx <1004945442@qq.com>
2016-12-06 10:27:15 +0800

    update

--- quadWater/index.js
+++ quadWater/index.js
@@ -2,8 +2,10 @@ import Promise from 'bluebird';
 import quadScene from './quadScene';
 
 window.audioTag=document.querySelector('audio')
+
 var t=new quadScene(document.querySelector('.container canvas'));
 t.load().delay(600).then(function(argument) {
 	t._checkSize();
+	t.resize(window.innerWidth,window.innerHeight)
 	t.play()
 })
