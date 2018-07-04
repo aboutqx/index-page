084b13f828803a7fdfab3c7953c19c6ee85c3e1b
zhoujiang <zhoujiang@zhaozhi.me>
2018-07-04 14:33:59 +0800

    update resume

--- resume/dist/resume.d71df78f.js
+++ resume/dist/resume.d71df78f.js
@@ -210,7 +210,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '21680' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '42212' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
